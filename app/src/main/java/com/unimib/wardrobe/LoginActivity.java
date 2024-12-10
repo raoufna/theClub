a5365af0bbba13930520e4068fc5321c29cb6df7
siimoneRadaelli <s.radaelli37@campus.unimib.it>
2024-12-10 17:18:09 +0100

    finito il check per controllare che email e password siano corrette, con annessi messaggi di errore

--- app/src/main/java/com/unimib/wardrobe/LoginActivity.java
+++ app/src/main/java/com/unimib/wardrobe/LoginActivity.java
@@ -10,9 +10,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.apache.commons.validator.routines.EmailValidator;
 
 public class LoginActivity extends AppCompatActivity {
     private TextInputEditText editTextEmail, editTextPassword;
+
     public static final String TAG = LoginActivity.class.getName();
 
 
@@ -23,14 +27,40 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
         editTextEmail = findViewById(R.id.email);
+        TextInputLayout passwordInputLayout = findViewById(R.id.passwordInputLayout);
         editTextPassword = findViewById(R.id.password);
+        TextInputLayout emailInputLayout = findViewById(R.id.emailInputLayout);
+
         Button loginButton = findViewById(R.id.filledButton);
         loginButton.setOnClickListener(view -> {
+            if(isEmailOk(editTextEmail.getText().toString())){
+                emailInputLayout.setError(null);
+                emailInputLayout.setHintTextColor(null);
 
+                if (isPasswordOk(editTextPassword.getText().toString())) {
+                    passwordInputLayout.setError(null);
+                    passwordInputLayout.setHintTextColor(null);
+                }else{
+                    passwordInputLayout.setEndIconMode(TextInputLayout.END_ICON_NONE);
+                    passwordInputLayout.setError(" ");
+                    passwordInputLayout.setErrorIconDrawable(null);
+                    passwordInputLayout.setHintTextColor(getResources().getColorStateList(R.color.md_theme_error));
+                    editTextPassword.setError("la password deve avere almeno 8 caratteri");
+                }
+
+            }else{
+                emailInputLayout.setError(" ");
+                emailInputLayout.setErrorIconDrawable(null);
+                emailInputLayout.setHintTextColor(getResources().getColorStateList(R.color.md_theme_error));
+                editTextEmail.setError("l'email non è valida");
+            }
         });
 
     }
-    private boolean passwordIsOk(String password){
+    private boolean isEmailOk(String email){
+        return EmailValidator.getInstance().isValid(email);
+    }
+    private boolean isPasswordOk(String password){
         return password.length() > 7;
     }
 }
