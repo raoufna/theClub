99ef8e96c182f414bb0c491f9503944f4e443a8a
siimoneRadaelli <s.radaelli37@campus.unimib.it>
2025-03-13 17:13:42 +0100

    modificata la home_fragment con il json

--- app/src/main/java/com/unimib/wardrobe/adapter/ProductRecycleAdapter.java
+++ app/src/main/java/com/unimib/wardrobe/adapter/ProductRecycleAdapter.java
@@ -6,7 +6,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
-import com.unimib.wardrobe.model.Product
+import com.unimib.wardrobe.model.Product;
 import com.unimib.wardrobe.R;
 import com.unimib.wardrobe.model.Product;
 
@@ -19,19 +19,26 @@ public class ProductRecycleAdapter extends RecyclerView.Adapter<ProductRecycleAd
         private List<Product> productList;
 
         public static class ViewHolder extends RecyclerView.ViewHolder {
-            private final TextView textView;
+            private final TextView TextViewTitle;
+            private final TextView TextViewDescription;
 
             public ViewHolder(View view) {
                 super(view);
                 // Define click listener for the ViewHolder's View
 
-                textView = (TextView) view.findViewById(R.id.textView);
+                TextViewTitle = view.findViewById(R.id.TextViewTitle);
+                TextViewDescription = view.findViewById(R.id.TextViewDescription);
             }
 
-            public TextView getTextView() {
-                return textView;
+            public TextView getTextViewTitle() {
+                return TextViewTitle;
+            }
+
+            public TextView getTextViewDescription() {
+                return TextViewDescription;
             }
         }
+
 
         public ProductRecycleAdapter(int layout, List<Product> productList) {
             this.layout = layout;
@@ -51,15 +58,13 @@ public class ProductRecycleAdapter extends RecyclerView.Adapter<ProductRecycleAd
         // Replace the contents of a view (invoked by the layout manager)
         @Override
         public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-
-            // Get element from your dataset at this position and replace the
-            // contents of the view with that element
-            viewHolder.getTextView().setText(localDataSet[position]);
+            viewHolder.getTextViewTitle().setText(productList.get(position).getName());
+            viewHolder.getTextViewDescription().setText(productList.get(position).getBrandName());
         }
 
         // Return the size of your dataset (invoked by the layout manager)
         @Override
         public int getItemCount() {
-            return localDataSet.length;
+            return productList.size();
         }
     }
--- app/src/main/java/com/unimib/wardrobe/ui/home/fragments/homeFragment.java
+++ app/src/main/java/com/unimib/wardrobe/ui/home/fragments/homeFragment.java
@@ -3,12 +3,16 @@ package com.unimib.wardrobe.ui.home.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.unimib.wardrobe.R;
+import com.unimib.wardrobe.adapter.ProductRecycleAdapter;
 import com.unimib.wardrobe.model.Product;
 import com.unimib.wardrobe.model.ProductAPIResponse;
 import com.unimib.wardrobe.util.Constants;
@@ -17,11 +21,6 @@ import com.unimib.wardrobe.util.JSONParserUtils;
 import java.io.IOException;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link homeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class homeFragment extends Fragment {
 
     public static final String TAG = homeFragment.class.getName();
@@ -38,10 +37,18 @@ public class homeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycleView);
+        //recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setLayoutManager(new GridLayoutManager(view.getContext(), 2));
         JSONParserUtils jsonParserUtils = new JSONParserUtils(getContext());
         try {
             ProductAPIResponse response = jsonParserUtils.parseJSONFileWithGSon(Constants.JsonWardrobe);
             List<Product> productList = response.getProducts();
+
+            ProductRecycleAdapter adapter = new ProductRecycleAdapter(R.layout.card_item, productList);
+
+            recyclerView.setAdapter(adapter);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
