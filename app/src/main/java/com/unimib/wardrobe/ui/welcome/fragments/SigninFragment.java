27d63a9042ba621e8aa5e1aa828286f360ed1321
ShdwMnrc <a.ranieri11@campus.unimib.it>
2025-03-27 13:30:01 +0100

    sign up fragment aggiunto, bottoni funzinanti, da aggiungere controlli e api google per accesso

--- app/src/main/java/com/unimib/wardrobe/ui/welcome/fragments/SigninFragment.java
+++ app/src/main/java/com/unimib/wardrobe/ui/welcome/fragments/SigninFragment.java
@@ -2,11 +2,15 @@ package com.unimib.wardrobe.ui.welcome.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.unimib.wardrobe.R;
 
@@ -63,4 +67,19 @@ public class SigninFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signin, container, false);
     }
-}
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Button SignupButton = view.findViewById(R.id.SignupButton);
+        SignupButton.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_signinFragment_to_mainActivity);
+        });
+        Button textButtonlog = view.findViewById(R.id.textButtonlog);
+        textButtonlog.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_signinFragment_to_loginFragment);
+        });
+        }
+    }
+
+
+
